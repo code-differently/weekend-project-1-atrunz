9b3a8f354d7ddcf9a48f1678fed174b642007f10
atrunz <atrunzo261@gmail.com>
2026-02-06 14:16:14 -0500

    fixed rounding in main

--- src/main/java/org/codedifferently/ReceiptApp/Main.java
+++ src/main/java/org/codedifferently/ReceiptApp/Main.java
@@ -77,30 +77,48 @@ public class Main {
         System.out.println("--------- Welcome to Alex's Electronics ----------");
         System.out.println("Visit ID: " + visitID);
         System.out.println("Receipt Code: " + fullReceiptCode);
-        System.out.println(item1.getName() + ": $" + item1.getPrice());
-        System.out.println(item2.getName() + ": $" + item2.getPrice());
-        System.out.println(item3.getName() + ": $" + item3.getPrice());
-        System.out.println("Subtotal: $" + subTotal);
-        System.out.println("Sales Tax: " + taxRate +"%");
+        //System.out.println(item1.getName() + ": $" + item1.getPrice());
+        System.out.println(item1.getName() + ": $" + Math.round(item1.getPrice() * 100.0) / 100.0);
+       // System.out.println(item2.getName() + ": $" + item2.getPrice());
+        System.out.println(item2.getName() + ": $" + Math.round(item2.getPrice() * 100.0) / 100.0);
+        //System.out.println(item3.getName() + ": $" + item3.getPrice());
+        System.out.println(item3.getName() + ": $" + Math.round(item3.getPrice() * 100.0) / 100.0);
+        //System.out.println("Subtotal: $" + subTotal);
+        System.out.println("Subtotal: $" + Math.round(subTotal * 100.0) / 100.0);
+        //System.out.println("Sales Tax: " + taxRate +"%");
+        System.out.println("Sales Tax: " + Math.round(taxRate * 100.0) / 100.0 + "%");
+
 
         if(isDiscount){
             System.out.println("--------- You received a lucky discount! ---------");
-            System.out.println("Lucky Discount: " + discountRate + "%");
+            //System.out.println("Lucky Discount: " + discountRate + "%");
+            System.out.println("Lucky Discount: " + (Math.round(discountRate * 100.0)/100.0) + "%");
+
             subTotal = calculatePrice.extraValues(subTotal, true, 0, discountRate);
-            System.out.println("New Subtotal: $" + subTotal);
+            //System.out.println("New Subtotal: $" + subTotal);
+            System.out.println("New Subtotal: $" + (Math.round(subTotal*100.0)/100.0));
+
             //System.out.println("You will receive: " + subTotal*(discountRate/100) + " off");
 
         }else{
             System.out.println("--------- A small processing fee was added to your order---------");
-            System.out.println("Processing Fee: " + "$" + fee);
+            //System.out.println("Processing Fee: " + "$" + fee);
+            System.out.println("Processing Fee: " + "$" + (Math.round(fee * 100.0)/100.0));
+
             subTotal =  calculatePrice.extraValues(subTotal, false, fee, 0);
-            System.out.println("New Subtotal: $" + subTotal);
+            //System.out.println("New Subtotal: $" + subTotal);
+            System.out.println("New Subtotal: $" + (Math.round(subTotal*100.0)/100.0));
+
         }
         if(isValidCoupon){
             System.out.println("---------- Coupon Code *" + couponCode.toUpperCase() + "* Accepted ----------");
-            System.out.println("Discount: " + discountVal + "%");
+            //System.out.println("Discount: " + discountVal + "%");
+            System.out.println("Discount: " + (Math.round(discountVal * 100.0)/100.0) + "%");
+
             subTotal = calculatePrice.extraValues(subTotal, true, 0, discountVal);
-            System.out.println("New Subtotal: $" + subTotal);
+            //System.out.println("New Subtotal: $" + subTotal);
+            System.out.println("New Subtotal: $" + (Math.round(subTotal*100.0)/100.0));
+
             //System.out.println("Your will receive: " + subTotal*(discountVal/100) + " off");
         }
         System.out.println("Final Total: $" + finalTotal);
