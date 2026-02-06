2688b8180b39642a68c15294e66a2d04b33efd38
atrunz <atrunzo261@gmail.com>
2026-02-05 22:08:10 -0500

    main partially completed

--- src/main/java/org/codedifferently/ReceiptApp/Main.java
+++ src/main/java/org/codedifferently/ReceiptApp/Main.java
@@ -1,35 +1,57 @@
 package org.codedifferently.ReceiptApp;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
 
+        String[] couponCodes = {"25OFF", "50OFF", "75OFF", "100OFF"};
+        System.out.println("Please enter your username: ");
+        Scanner scanner = new Scanner(System.in);
 
-        //System.out.println("Please enter your username: ");
-        //Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
 
-        //String username = scanner.nextLine();
+        System.out.println("Please enter your budget: ");
+        double budget = scanner.nextDouble();
 
-        //System.out.println("Please enter your budget: ");
-        //double budget = scanner.nextDouble();
+        System.out.println("Please enter your coupon code: ");
+        String couponCode = scanner.nextLine().toUpperCase();
 
-        // System.out.println("Please enter your coupon code: ");
-        // String couponCode = scanner.nextLine();
+        for (int i=0; i< couponCodes.length; i++){
+            if(couponCodes[i].equals(couponCode)){
+                //get a discount
+                break;
+            }
+            //don't get a discount
+        }
 
 
         RandomGenerations randomItem = new RandomGenerations();
+        PriceCalculations calculatePrice = new PriceCalculations();
 
         //generate random item IDs
         int itemSelectionOne = randomItem.item();
         int itemSelectionTwo = randomItem.item();
         int itemSelectionThree = randomItem.item();
 
+        boolean isDiscount = randomItem.discount();
+        double fee = randomItem.fee();
+        double taxRate = randomItem.tax();
+        double discountRate = randomItem.tax();
+
         //generate random items for receipt
         StoreItem item1 = new StoreItem(itemSelectionOne, randomItem.prices());
         StoreItem item2 = new StoreItem(itemSelectionTwo, randomItem.prices());
         StoreItem item3 = new StoreItem(itemSelectionThree, randomItem.prices());
+
+        double[] itemPrices = {item1.getPrice(), item2.getPrice(), item3.getPrice()};
+
+        double subTotal = calculatePrice.subtotal(itemPrices);
+
+        double finalTotal = calculatePrice.finalTotal(subTotal, isDiscount, fee, taxRate, discountRate);
 
         System.out.println(item1.getName() + item1.getPrice());
         System.out.println(item2.getName() + item2.getPrice());
